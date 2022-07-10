f6c526eb7515b4b526a89217ae70589b7226816b
VladNCode <vldsmrfow@gmail.com>
2022-07-10 17:54:05 +0300

    changed mongo config to suit mongo atlas uri

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -2,6 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 
+export class RegisterDto {
+	email: string;
+	password: string;
+	displayName?: string;
+}
+
+export class LoginDto {
+	email: string;
+	password: string;
+}
+
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
@@ -17,15 +28,4 @@ export class AuthController {
 
 		return this.authService.login(id);
 	}
-}
-
-export class RegisterDto {
-	email: string;
-	password: string;
-	displayName?: string;
-}
-
-export class LoginDto {
-	email: string;
-	password: string;
 }
